683d36625852804b96de740f756b2abb8b1e2f0f
Char Wae (Erica) Chong <57076488+chonger878@users.noreply.github.com>
2021-02-03 21:26:11 -0800

    updated createVMActionFile.ps1 file

--- getVMFiles/createVMActionFile.ps1
+++ getVMFiles/createVMActionFile.ps1
@@ -2,10 +2,10 @@
 #region getLogin
 #Write-Output "Service Principal Login to Azure"
 az login `
-    --service-principal `
-    --username $servicePrincipal `
-    --password $servicePrincipalSecret `
-    --tenant $servicePrincipalTenantId 
+    -service-principal `
+    -username $servicePrincipal `
+    -password $servicePrincipalSecret `
+    -tenant $servicePrincipalTenantId 
 Write-Output "Logged In"
 Write-Output ""
 #endregion
@@ -14,7 +14,7 @@ Write-Output ""
 #Gets the right subscription to ensure the data goes to the right place
 Write-Output  "Set Default for Azure Subscription"
 az account set `
-    -- subscription $azureSubscriptionName
+    - subscription $azureSubscriptionName
 Write-Output "Default Subscription set"
 Write-Output ""
 #endregion
@@ -24,11 +24,11 @@ Write-Output ""
 Write-Output "Creating VM in Az"
 try{
     az vm create
-    --resource-group $resourceGroupName
-    --name $serverName `
-    --image win2016datacenter `
-    --admin-username $adminLogin `
-    --admin-password $adminPassword
+    -resource-group $resourceGroupName
+    -name $serverName `
+    -image win2019datacenter `
+    -admin-username $adminLogin `
+    -admin-password $adminPassword
   }
 catch {
     Write-Output "VM already exists in Azure"
