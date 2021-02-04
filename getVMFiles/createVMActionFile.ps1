83637e08f54fb435a4e0fa0e93381f8e19b7e4f7
Char Wae (Erica) Chong <57076488+chonger878@users.noreply.github.com>
2021-02-03 20:36:38 -0800

    docs for test run to create VM

---
+++ getVMFiles/createVMActionFile.ps1
@@ -0,0 +1,39 @@
+#region getLogin
+#Write-Output "Service Principal Login to Azure"
+az login
+    --service-principal `
+    --username $servicePrincipal `
+    --password $servicePrincipalSecret `
+    --tenant $servicePrincipalTenantId 
+Write-Output "Logged In"
+Write-Ourput ""
+#endregion
+
+#region getSubscription
+#Gets the right subscription to ensure the data goes to the right place
+Write-Output  "Set Default for Azure Subscription"
+az account set `
+    -- subscription $azureSubscriptionName
+Write-Output "Default Subscription set"
+Write-Output ""
+#endregion
+
+#region Create VM
+#Creates a VM within a resource group
+Write-Output "Creating VM in Az"
+try{
+    az vm create
+    --resource-group $resourceGroupName
+    --name $serverName `
+    --image win2016datacenter `
+    --admin-username $adminLogin `
+    --admin-password $adminPassword
+  }
+catch {
+    Write-Output "VM already exists in Azure"
+    }
+Write-Output "VM created"
+write-Output ""
+#endregion
+
+
